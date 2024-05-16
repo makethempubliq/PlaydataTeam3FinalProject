2c592f0b7edb287fd3505b97a4dfad4c2c2d6853
Sophie Kwon <mmloo0525@gmail.com>
2024-05-16 12:11:05 +0900

    0516 CSS 01

--- mrs_spring_web/src/main/resources/PodTalk-1.0.0/js/playlist.js
+++ mrs_spring_web/src/main/resources/PodTalk-1.0.0/js/playlist.js
@@ -1,4 +1,4 @@
-// 플레이 아이콘에 대한 클릭 이벤트 처리
+
 document.getElementById("playIcon").addEventListener("click", function () {
     var playIcon = document.getElementById("playIcon");
     if (playIcon.classList.contains("bi-play-circle")) {
@@ -9,8 +9,6 @@ document.getElementById("playIcon").addEventListener("click", function () {
         playIcon.classList.add("bi-play-circle");
     }
 });
-
-// 하트 아이콘에 대한 클릭 이벤트 처리
 
 document.getElementById("heartIcon").addEventListener("click", function () {
     var heartIcon = document.getElementById("heartIcon");
@@ -24,6 +22,46 @@ document.getElementById("heartIcon").addEventListener("click", function () {
 });
 
 
+
+
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+
+
+// 시간을 초 단위로 변환하는 함수
+function timeToSeconds(time) {
+    var parts = time.split(':');
+    return parseInt(parts[0]) * 60 + parseInt(parts[1]);
+}
+
+// 각 요소의 시간을 가져와 총 시간을 계산하는 함수
+function calculateTotalDuration() {
+    var durations = document.querySelectorAll('.duration');
+    var totalSeconds = 0;
+    durations.forEach(function (duration) {
+        totalSeconds += timeToSeconds(duration.innerText);
+    });
+    return totalSeconds;
+}
+
+// 총 시간을 표시하는 함수
+function displayTotalDuration() {
+    var totalSeconds = calculateTotalDuration();
+    var minutes = Math.floor(totalSeconds / 60);
+    var seconds = totalSeconds % 60;
+    document.getElementById('totalDuration').textContent = minutes + '분' + ' ' + seconds + '초';
+}
+
+// 총 곡 수를 표시하는 함수
+function displayTotalSongs() {
+    var totalSongs = document.querySelectorAll('.custom-list-group-item').length;
+    document.getElementById('totalSongs').textContent = '총 ' + totalSongs + '곡,';
+}
+
+// 페이지 로드시 총 시간과 총 곡 수 표시
+window.onload = function () {
+    displayTotalDuration();
+    displayTotalSongs();
+};

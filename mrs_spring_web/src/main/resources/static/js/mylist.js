2accf99cac4a172296e49dd7d032ac3a24599c90
Sophie Kwon <mmloo0525@gmail.com>
2024-05-17 12:28:30 +0900

    0517

--- mrs_spring_web/src/main/resources/static/js/mylist.js
+++ mrs_spring_web/src/main/resources/static/js/mylist.js
@@ -9,3 +9,30 @@ function confirmDelete() {
     } else {
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const playIcons = document.querySelectorAll('#playIcon');
+    const playerBar = document.getElementById('player-bar');
+    const nowPlaying = document.getElementById('now-playing');
+    const audioPlayer = document.getElementById('audio-player');
+    const audioSource = document.getElementById('audio-source');
+    const closePlayerBarButton = document.getElementById('close-player-bar');
+
+    playIcons.forEach(icon => {
+        icon.addEventListener('click', function () {
+            // const playlistName = this.closest('li').querySelector('button').textContent;
+            // // This is where you'd set the audio source URL dynamically if needed
+            // const audioUrl = 'your-audio-file-url.mp3';
+
+            nowPlaying.textContent = playlistName;
+            audioSource.src = audioUrl;
+            audioPlayer.load();
+            playerBar.style.display = 'block';
+        });
+    });
+
+    closePlayerBarButton.addEventListener('click', function () {
+        playerBar.style.display = 'none';
+        audioPlayer.pause();
+    });
+});

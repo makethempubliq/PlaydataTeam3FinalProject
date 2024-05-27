c3b2851361ae2deef46d3d4099b6527a6cc0c839
Sophie Kwon <mmloo0525@gmail.com>
2024-05-27 17:14:24 +0900

    0527 CSS

--- mrs_spring_web/src/main/java/com/example/mrs_spring_web/Controller/UserController.java
+++ mrs_spring_web/src/main/java/com/example/mrs_spring_web/Controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.mrs_spring_web.Controller;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,19 +36,23 @@ public class UserController {
     }
 
     @GetMapping("/main")
-    public String main(Authentication authentication, @AuthenticationPrincipal UserDetails userDetailsObj, Model model) throws Exception{
+    public String main(Authentication authentication, @AuthenticationPrincipal UserDetails userDetailsObj, Model model)
+            throws Exception {
         User currentUser = spotifyService.getUserProfile(userDetailsObj.getUsername());
         model.addAttribute("username", currentUser.getDisplayName());
-        model.addAttribute("koreatoptrackart", spotifyService.getPlaylistArt(userDetailsObj.getUsername(), "37i9dQZEVXbNxXF4SkHj9F").getUrl());
-        model.addAttribute("globaltoptrackart", spotifyService.getPlaylistArt(userDetailsObj.getUsername(), "37i9dQZEVXbMDoHDwVN2tF").getUrl());
+        model.addAttribute("koreatoptrackart",
+                spotifyService.getPlaylistArt(userDetailsObj.getUsername(), "37i9dQZEVXbNxXF4SkHj9F").getUrl());
+        model.addAttribute("globaltoptrackart",
+                spotifyService.getPlaylistArt(userDetailsObj.getUsername(), "37i9dQZEVXbMDoHDwVN2tF").getUrl());
         return "main";
     }
 
     @GetMapping("/musicplayer")
-    public String musicplayer(@RequestParam("id") String playlistId, Authentication authentication, @AuthenticationPrincipal UserDetails userDetailsObj, Model model) throws Exception{
+    public String musicplayer(@RequestParam("id") String playlistId, Authentication authentication,
+            @AuthenticationPrincipal UserDetails userDetailsObj, Model model) throws Exception {
         List<String> playlistTracks = spotifyService.getTracklistByPlaylist(userDetailsObj.getUsername(), playlistId);
         String accesstoken = userService.getAccesstoken(userDetailsObj.getUsername());
-        
+
         model.addAttribute("accesstoken", accesstoken);
         model.addAttribute("tracklist", playlistTracks);
         model.addAttribute("playlistTracks", playlistTracks.toString());
@@ -57,28 +60,37 @@ public class UserController {
     }
 
     @GetMapping("/playlist")
-    public String showplaylist(@RequestParam(value = "id", required = false) String playlistId, @RequestParam(value = "recommendedtracks", required = false) String recommendedtracks, @RequestParam(value = "tokenizedTheme", required = false) String themes, Authentication authentication, @AuthenticationPrincipal UserDetails userDetailsObj, Model model) throws Exception{
-        
+    public String showplaylist(@RequestParam(value = "id", required = false) String playlistId,
+            @RequestParam(value = "recommendedtracks", required = false) String recommendedtracks,
+            @RequestParam(value = "tokenizedTheme", required = false) String themes, Authentication authentication,
+            @AuthenticationPrincipal UserDetails userDetailsObj, Model model) throws Exception {
+
         String accesstoken = userService.getAccesstoken(userDetailsObj.getUsername());
-        if (playlistId != null){
+        if (playlistId != null) {
             List<String> Tracks = spotifyService.getTracklistByPlaylist(userDetailsObj.getUsername(), playlistId);
-            model.addAttribute("playlistArt", spotifyService.getPlaylistArt(userDetailsObj.getUsername(), playlistId).getUrl());
+            model.addAttribute("playlistArt",
+                    spotifyService.getPlaylistArt(userDetailsObj.getUsername(), playlistId).getUrl());
             model.addAttribute("tracklist", Tracks);
             model.addAttribute("trackDataList", spotifyService.getTracksdata(userDetailsObj.getUsername(), Tracks));
-        }
-        else {
-            List<String> Tracks = new Gson().fromJson(recommendedtracks, new TypeToken<List<String>>() {}.getType());
+        } else {
+            List<String> Tracks = new Gson().fromJson(recommendedtracks, new TypeToken<List<String>>() {
+            }.getType());
             model.addAttribute("tracklist", Tracks);
             model.addAttribute("trackDataList", spotifyService.getTracksdata(userDetailsObj.getUsername(), Tracks));
-            model.addAttribute("themes", new Gson().fromJson(themes, new TypeToken<List<String>>() {}.getType()));
+            model.addAttribute("themes", new Gson().fromJson(themes, new TypeToken<List<String>>() {
+            }.getType()));
         }
         model.addAttribute("accesstoken", accesstoken);
+
+        User currentUser = spotifyService.getUserProfile(userDetailsObj.getUsername());
+        model.addAttribute("username", currentUser.getDisplayName());
         return "playlist";
     }
 
     @GetMapping("/playerbar")
-    public String showplayerbar(@RequestParam String trackdata, Authentication authentication, @AuthenticationPrincipal UserDetails userDetailsObj, Model model) throws Exception{
-        
+    public String showplayerbar(@RequestParam String trackdata, Authentication authentication,
+            @AuthenticationPrincipal UserDetails userDetailsObj, Model model) throws Exception {
+
         String accesstoken = userService.getAccesstoken(userDetailsObj.getUsername());
         spotifyService.getCurrentPlayState(userDetailsObj.getUsername());
         List<String> tracksList = new ArrayList<>();
@@ -89,16 +101,16 @@ public class UserController {
         for (String track : tracksArray) {
             tracksList.add(track);
         }
-        log.info("show playerbar : trackdata : "+trackdata + accesstoken);
+        log.info("show playerbar : trackdata : " + trackdata + accesstoken);
         model.addAttribute("accesstoken", accesstoken);
         model.addAttribute("tracklist", tracksList);
         model.addAttribute("trackDataList", spotifyService.getTracksdata(userDetailsObj.getUsername(), tracksList));
         return "playerbar";
     }
-    
 
     @GetMapping("/makeplaylist")
-    public String makeplaylist(Authentication authentication, @AuthenticationPrincipal UserDetails userDetailsObj, Model model) {
+    public String makeplaylist(Authentication authentication, @AuthenticationPrincipal UserDetails userDetailsObj,
+            Model model) {
         log.info("[UserController] makeplaylist start!!");
         return "themeselect";
     }

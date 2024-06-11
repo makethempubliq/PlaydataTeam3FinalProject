fa20e6ae976a03a8f3169505885372326f3ed36c
신원식 <action_123@naver.com>
2024-06-11 19:06:25 +0900

    스프링 배포 준비

--- mrs_spring_web/src/main/resources/static/js/themepage.js
+++ mrs_spring_web/src/main/resources/static/js/themepage.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
             totalDuration: totalduration
         };
 
-        fetch("http://localhost:5000/api/v1/flask/themeselect", {
+        fetch("http://3.35.141.154:5000/api/v1/flask/themeselect", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 };
 
                 // Send the second POST request to get recommended tracks
-                return fetch("http://localhost:5000/api/v1/flask/gettracks", {
+                return fetch("http://3.35.141.154:5000/api/v1/flask/gettracks", {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'

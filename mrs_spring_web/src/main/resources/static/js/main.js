dabc9beb07cb99f5bb3b404954850eb116ee1add
신원식 <action_123@naver.com>
2024-06-13 20:21:39 +0900

    1

--- mrs_spring_web/src/main/resources/static/js/main.js
+++ mrs_spring_web/src/main/resources/static/js/main.js
@@ -7,7 +7,7 @@ function presettheme(tags) {
     };
 
     // Send the second POST request to get recommended tracks
-    fetch("//13.209.70.145:5000/api/v1/flask/gettracks", {
+    fetch("http://model.anonyq.site/api/v1/flask/gettracks", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
--- mrs_spring_web/src/main/resources/static/js/themepage.js
+++ mrs_spring_web/src/main/resources/static/js/themepage.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
             totalDuration: totalduration
         };
 
-        fetch("//13.209.70.145:5000/api/v1/flask/themeselect", {
+        fetch("http://model.anonyq.site/api/v1/flask/themeselect", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 };
 
                 // Send the second POST request to get recommended tracks
-                return fetch("//13.209.70.145:5000/api/v1/flask/gettracks", {
+                return fetch("http://model.anonyq.site/api/v1/flask/gettracks", {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
